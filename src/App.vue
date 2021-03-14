28f2e790cd5ad4613c606a66009bb72e8b204d82
Linus Styrén <linus122xbb@gmail.com>
2021-03-14 21:56:14 +0100

    flytta html och css till CitySearchField.vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,18 @@
 <template>
   <div>
     <h1>Weather App</h1>
-    <div class="flex">
-      <!--
-        type="search" och type="text" gör ingen skillnad men det är tydligare
-        att detta inputfält är ett sökfält. Se också:
-        https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/search
-      -->      
-      <input data-cy="city" class="input" type="search" placeholder="Köttkulla..">
-      <button data-cy="search">🔍</button>
-    </div>
+    <CitySearchField />
+    <CityInfoCard />
   </div>
 </template>
 
 <script>
+import CityInfoCard from './components/CityInfoCard.vue';
+import CitySearchField from './components/CitySearchField.vue';
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: { CityInfoCard, CitySearchField }
 }
 </script>
 
@@ -31,17 +28,5 @@ export default {
   justify-content: center;
   height: calc(100vh - 5em);
   font-size: 24px;
-}
-
-.flex {
-  display: flex;
-  max-width: 400px;
-}
-
-.input {
-  width: 100%;
-  padding: 8px 10px;
-  border: 1px solid #32485F;
-  flex: 1;
 }
 </style>
--- src/components/CitySearchField.vue
+++ src/components/CitySearchField.vue
@@ -1,5 +1,13 @@
 <template>
-  <div></div>
+  <div class="flex">
+    <!--
+      type="search" och type="text" gör ingen skillnad men det är tydligare
+      att detta inputfält är ett sökfält. Se också:
+      https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/search
+    -->      
+    <input data-cy="city" class="input" type="search" placeholder="Köttkulla..">
+    <button data-cy="search">🔍</button>
+  </div>
 </template>
 
 <script>
@@ -9,5 +17,15 @@ export default {
 </script>
 
 <style>
+.flex {
+  display: flex;
+  max-width: 400px;
+}
 
+.input {
+  width: 100%;
+  padding: 8px 10px;
+  border: 1px solid #32485F;
+  flex: 1;
+}
 </style>
