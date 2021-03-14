578cda63a76b9434782bec28a430738fde372c7f
Linus Styrén <linus122xbb@gmail.com>
2021-03-14 23:55:19 +0100

    lägg till grid layout för CityInfoCard.vue

--- src/components/CityInfoCard.vue
+++ src/components/CityInfoCard.vue
@@ -36,9 +36,29 @@ export default {
   .card {
     display: grid;
     min-width: 400px;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: 2fr 1fr;
+    grid-template-areas:
+      "sm wt"
+      "dt dt";
     background-color: rgba(232, 230, 227, 0.87);
     border-radius: 2em;
     padding: 2em;
     box-shadow: rgba(0, 0, 0, 0.3) 0px 0px 10px 1px;
+  }
+
+  .summary {
+    background-color: red;
+    grid-area: sm;
+  }
+
+  .weather {
+    background-color: green;
+    grid-area: wt;
+  }
+
+  .details {
+    background-color: blue;
+    grid-area: dt;
   }
 </style>
